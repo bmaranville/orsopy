f2be2ecd50581ff2b3433e8ea1d67f6ce04fe104
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-04-15 14:45:31 +0000

    From refs/heads/main 831e5a245c0b7f5c087007b48d137f5d461914b9

---
+++ README.md
@@ -0,0 +1,3 @@
+#GitHub Pages
+
+Last update of sphinx html documentation from [831e5a2](https://github.com/bmaranville/orsopy/tree/831e5a245c0b7f5c087007b48d137f5d461914b9)
--- orsopy/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Top-level package for orsopy."""
-
-__version__ = "1.2.0"
-
-# this is imported in submodules to be able to overwrite it with pydantic
-from dataclasses import dataclass
